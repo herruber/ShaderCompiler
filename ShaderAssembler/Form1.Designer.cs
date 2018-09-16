68ed7f7139e05bb749ac40b71d7b035c71cfdcae
herruber <asteroth69@hotmail.com>
2018-09-16 22:17:05 +0200

    First commit

--- ShaderAssembler/Form1.Designer.cs
+++ ShaderAssembler/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pickShader = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.txt_Visual = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // pickShader
+            // 
+            this.pickShader.Location = new System.Drawing.Point(12, 12);
+            this.pickShader.Name = "pickShader";
+            this.pickShader.Size = new System.Drawing.Size(153, 59);
+            this.pickShader.TabIndex = 0;
+            this.pickShader.Text = "Pick Shader..";
+            this.pickShader.UseVisualStyleBackColor = true;
+            this.pickShader.Click += new System.EventHandler(this.pickShader_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(12, 93);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(156, 59);
+            this.save.TabIndex = 1;
+            this.save.Text = "Save..";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // txt_Visual
+            // 
+            this.txt_Visual.Location = new System.Drawing.Point(257, 12);
+            this.txt_Visual.Name = "txt_Visual";
+            this.txt_Visual.Size = new System.Drawing.Size(744, 492);
+            this.txt_Visual.TabIndex = 2;
+            this.txt_Visual.Text = "";
+            this.txt_Visual.TextChanged += new System.EventHandler(this.txt_Visual_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 516);
+            this.Controls.Add(this.txt_Visual);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.pickShader);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button pickShader;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.RichTextBox txt_Visual;
     }
 }
 
--- ShaderAssembler/Form1.cs
+++ ShaderAssembler/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShaderAssembler;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,148 @@ using System.Windows.Forms;
 
 namespace ShaderAssembler
 {
+
     public partial class Form1 : Form
     {
+
+        public static string destination = @"C:\Repos\Arctic Wood\ConfiguratorBasic\ConfiguratorBasic\Shaders";
+        public static string chunkLib = @"C:\Users\Grävling\Downloads\three.js-dev\src\renderers\shaders\ShaderChunk";
+        public ThreeShader vertex_shader;
+        public ThreeShader fragment_shader;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void pickShader_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.InitialDirectory = @"C:\Users\Grävling\Downloads\three.js-dev\src\renderers\shaders\ShaderLib";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txt_Visual.Clear();
+                    var p = dlg.FileName.Split('_')[0];
+                    var vert = p + "_vert.glsl";
+                    var frag = p + "_frag.glsl";
+                    vertex_shader = new ThreeShader(vert);
+                    fragment_shader = new ThreeShader(frag);
+
+                }
+            }
+
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                dlg.SelectedPath = destination;
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+
+                    var vertsource = vertex_shader.Parse(chunkLib, txt_Visual);
+                    var fragsource = fragment_shader.Parse(chunkLib, txt_Visual);
+
+                    System.IO.File.WriteAllLines( dlg.SelectedPath + "/" + System.IO.Path.GetFileName(vertex_shader.FilePath) , vertsource);
+                    System.IO.File.WriteAllLines(dlg.SelectedPath + "/" + System.IO.Path.GetFileName(fragment_shader.FilePath), fragsource);
+
+                }
+            }
+        }
+
+        private void txt_Visual_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
+
+    public class ThreeShader
+    {
+        public string FilePath { get; set; }
+        public string Name { get; set; }
+        public List<string> Chunks { get; set; }
+
+
+        public ThreeShader(string path)
+        {
+            FilePath = path;
+            Name = System.IO.Path.GetFileNameWithoutExtension(path).Split('_')[0];
+        }
+
+        public List<string> Parse(string chunkLib, RichTextBox txt)
+        {
+
+            string[] paths = System.IO.Directory.GetFiles(chunkLib);
+            string[] lines = System.IO.File.ReadAllLines(FilePath);
+            List<string> print = new List<string>();
+
+            txt.AppendText("\r\n" + System.IO.Path.GetFileNameWithoutExtension(FilePath));
+
+            foreach (var line in lines)
+            {
+                txt.AppendText("\r\n");
+
+                if (!line.Contains("include"))
+                {
+                    print.Add(line);
+                    txt.AppendText(line);
+                }
+                else
+                {
+
+                    string formated = line.Split(' ')[1]; //Remove #include
+                    formated = formated.Remove(0, 1);
+                    formated = formated.Remove(formated.Length - 1, 1);
+
+                    foreach (var path in paths)
+                    {
+
+                        if (path.Contains(formated))
+                        {
+
+                            int start = txt.TextLength;
+                            txt.AppendText(line + "\r\n");
+                            int end = txt.TextLength;
+
+                            txt.Select(start, end - start);
+                            {
+                                txt.SelectionColor = Color.ForestGreen;
+                                // could set box.SelectionBackColor, box.SelectionFont too.
+                            }
+                            txt.SelectionLength = 0; // clear
+                            string[] chunks = System.IO.File.ReadAllLines(path);
+
+                            foreach (var chunk in chunks)
+                            {
+                                print.Add(chunk);
+                                txt.AppendText(chunk + "\r\n");
+                            }
+
+
+
+
+
+                            break;
+                        }
+
+                    }
+
+                }
+
+            }
+
+            return print;
+        }
+
+
+
+    }
+
 }
